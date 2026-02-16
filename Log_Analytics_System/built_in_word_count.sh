6025100cdc5bab6cf34cca87025504a79ea0ae87
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-02-16 10:42:39 +0530

    Built-in WordCount Execution

---
+++ Log_Analytics_System/built_in_word_count.sh
@@ -0,0 +1,22 @@
+#Phase 3:Distributed Processing Using Built-in MapReduce
+#Task 4: Built-in WordCount Execution
+
+echo "Built-in WordCount Execution"
+
+#Remove output directory if it already exists
+hdfs dfs -rm -r /logs/output 2>/dev/null
+
+#Hadoop built-in word count
+hadoop jar \
+/mnt/d/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-3.3.6.jar \
+wordcount \
+/logs/source/large_logfile.log \
+/logs/output
+
+#Verify output directory
+hdfs dfs -ls /logs/output
+
+#Display Output 
+hdfs dfs -head /logs/output/part-r-00000
+
+echo "WordCount Job Completed Successfully!"
